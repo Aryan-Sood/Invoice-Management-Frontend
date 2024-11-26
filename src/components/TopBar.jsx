3b62d0f147e5d3b2c5fb6f1de1ea077bf754ddb2
Aryan Sood <aryansood12@gmail.com>
2024-11-26 09:59:15 +0530

    button ui updates for states

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/topbar.css';
 import { useNavigate } from 'react-router-dom';
 import { deleteAllInvoices } from '../services/api';
@@ -6,16 +6,18 @@ import { deleteAllInvoices } from '../services/api';
 const TopBar = () => {
 
     const navigate = useNavigate();
+    const [deleteText, setDeleteText] = useState('Delete All')
 
     const handleDeleteInvoices = async() => {
-        await deleteAllInvoices();
+        setDeleteText('Deleting ...')
+        await deleteAllInvoices().then((response) => navigate('/'));
     }
 
   return (
     <div className="top-bar">
       <div className="top-bar-buttons">
         <button className="top-bar-button" onClick={()=>{navigate('/create')}}>Create</button>
-        <button className="top-bar-button" style={{backgroundColor: 'red'}} onClick={handleDeleteInvoices}>Delete All</button>
+        <button className="top-bar-button" style={{backgroundColor: 'red'}} onClick={handleDeleteInvoices}>{deleteText}</button>
       </div>
       <h1 className="top-bar-title">Invoice Management</h1>
     </div>
